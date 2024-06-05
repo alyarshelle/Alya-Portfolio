0388a5761632ccf52a2440e90a62e175adfaf81d
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2024-06-05 19:38:46 +0000

    reciped

--- recipe-site/src/LandingPage.js
+++ recipe-site/src/LandingPage.js
@@ -2,6 +2,13 @@
 import React from 'react';
 import Card from './Card';
 import spaghet from './Images/spaghetti.png';
+import chicktaco from './Images/chicktacos.png';
+import chipcookie from './Images/chocochip.png';
+import redvelvet from './Images/redvelvet.png';
+import tsochicken from './Images/tsochicken.png';
+import potatoes from './Images/potatoes.png';
+import chickpeacurry from './Images/chickpeacurry.png';
+import eggrollbowl from './Images/eggrollinbowl.png';
 import './LandingPage.css';
 
 function LandingPage() {
@@ -11,7 +18,49 @@ function LandingPage() {
       title: 'Spaghetti and Meatballs',
       description: "Spaghetti and meatballs is a classic comfort food that's easy to make and tastes great!",
     },
-    // Add more recipes here
+
+    {
+        image: chicktaco,
+        title: 'Chicken Tacos',
+        description: "Chicken tacos are a quick and easy meal that's perfect for busy weeknights.",
+    },
+
+    {
+        image: chipcookie,
+        title: 'Chocolate Chip Cookies',
+        description: "Chocolate chip cookies are a delicious treat that everyone loves!",
+    },
+
+    {
+        image: tsochicken,
+        title: 'General Tso Chicken',
+        description: "General Tso chicken is a popular Chinese takeout dish that's easy to make at home.",
+    },
+
+    {
+        image: potatoes,
+        title: 'Mashed Potatoes',
+        description: "Mashed potatoes are a classic side dish that pairs well with a variety of main courses.",
+    },
+
+    {
+        image: redvelvet,
+        title: 'Red Velvet Cake',
+        description: "Red velvet cake is a classic dessert that's perfect for any occasion.",
+    },
+
+    {
+        image: chickpeacurry,
+        title: 'Chickpea Curry',
+        description: "Chickpea curry is a flavorful vegetarian dish that's easy to make and tastes great!",
+    },
+
+    {
+        image: eggrollbowl,
+        title: 'Egg Roll in A Bowl',
+        description: "Egg roll in a bowl is a quick and easy meal that's packed with flavor!",
+    },
+
   ];
 
   return (
@@ -19,7 +68,7 @@ function LandingPage() {
       <header>
         <h1>Welcome to Crafty Kitchen</h1>
       </header>
-
+      <h2>Popular Recipes</h2>
       <div className="recipes">
         {recipes.map((recipe, index) => (
           <Card
@@ -29,6 +78,11 @@ function LandingPage() {
             description={recipe.description}
           />
         ))}
+      </div>
+
+      <h2>Recently Viewed</h2>
+
+      <div className="recipes">
       </div>
 
       <footer>
